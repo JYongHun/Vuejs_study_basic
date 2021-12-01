ab147753a69a56b4fadcf7a53cc1cebdc9e971df
YongHun <jauo40@gmail.com>
2021-12-01 17:02:49 +0900

    1차 기본틀 적용

--- src/main.js
+++ src/main.js
@@ -1,17 +1,4 @@
-// import { createApp } from 'vue'
-// import App from './App.vue'
-// createApp(App).mount('#app')
+import { createApp } from 'vue'
+import App from './App.vue'
 
-
-import Vue from "vue";
-import App from "../App";
-import VueDragscroll from "vue-dragscroll";
-
-Vue.use(VueDragscroll);
-
-/* eslint-disable no-new */
-new Vue({
-  el: "#app",
-  render: h => h(App)
-});
-
+createApp(App).mount('#app')
